4bfd4cd799004ee1644f97ee04c647001db77fda
Alexei Yashkov <alexei.yashkov@yashkov.org>
2022-06-05 16:18:21 -0400

    Use own instance of the context for each test

--- test/context.spec.cc
+++ test/context.spec.cc
@@ -4,51 +4,59 @@
 using namespace altea;
 using namespace std;
 
-static DefaultEventProcessor ep;
-
 static auto _ = describe("Context", [] {
-    it("should do nothing when no suites", [] {
-        Context context;
+    DefaultEventProcessor *ep;
+    Context *context;
 
-        context.run(&ep);
+    beforeEach([&] {
+        ep = new DefaultEventProcessor();
+        context = new Context();
+    });
+
+    afterEach([&] {
+        delete context;
+        context = nullptr;
+        delete ep;
+        ep = nullptr;
+    });
+
+    it("should do nothing when no suites", [&] {
+        context->run(ep);
 
         expect().nothing();
     });
 
-    it("should do nothing when it has empty NORMAL suites", [] {
-        Context context;
-
-        context.addSuite(__FILE__, __LINE__, NORMAL, "one", [] {});
-        context.addSuite(__FILE__, __LINE__, NORMAL, "two", [] {});
-        context.run(&ep);
+    it("should do nothing when it has empty NORMAL suites", [&] {
+        context->addSuite(__FILE__, __LINE__, NORMAL, "one", [] {});
+        context->addSuite(__FILE__, __LINE__, NORMAL, "two", [] {});
+        context->run(ep);
 
         expect().nothing();
     });
 
-    it("should run NORMAL tests when defined in NORMAL suites", [] {
-        Context context;
+    it("should run NORMAL tests when defined in NORMAL suites", [&] {
         int testCount = 0;
 
-        context.addSuite(__FILE__, __LINE__, NORMAL, "one", [&] {
-            context.addTest(__FILE__, __LINE__, NORMAL, "one of one", [&] {
+        context->addSuite(__FILE__, __LINE__, NORMAL, "one", [&] {
+            context->addTest(__FILE__, __LINE__, NORMAL, "one of one", [&] {
                 ++testCount;
 
-                context.doExpect(__FILE__, __LINE__).nothing();
+                context->doExpect(__FILE__, __LINE__).nothing();
             });
-            context.addTest(__FILE__, __LINE__, NORMAL, "two of one", [&] {
+            context->addTest(__FILE__, __LINE__, NORMAL, "two of one", [&] {
                 ++testCount;
 
-                context.doExpect(__FILE__, __LINE__).nothing();
-            });
-        });
-        context.addSuite(__FILE__, __LINE__, NORMAL, "two", [&] {
-            context.addTest(__FILE__, __LINE__, NORMAL, "one of one", [&] {
-                ++testCount;
-
-                context.doExpect(__FILE__, __LINE__).nothing();
+                context->doExpect(__FILE__, __LINE__).nothing();
             });
         });
-        context.run(&ep);
+        context->addSuite(__FILE__, __LINE__, NORMAL, "two", [&] {
+            context->addTest(__FILE__, __LINE__, NORMAL, "one of one", [&] {
+                ++testCount;
+
+                context->doExpect(__FILE__, __LINE__).nothing();
+            });
+        });
+        context->run(ep);
 
         expect(testCount == 3).toBeTrue();
     });
