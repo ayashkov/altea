5da16f3b5d917f3d3b2017847b6f2a7dd4bf7f71
Alexei Yashkov <alexei.yashkov@yashkov.org>
2022-06-05 19:13:46 -0400

    Some basic flow examples for normal tests and suites

--- test/context.spec.cc
+++ test/context.spec.cc
@@ -21,44 +21,70 @@ static auto _ = describe("Context", [] {
         delete ep;
     });
 
-    it("should do nothing when no suites", [&] {
+    it("should run to success when no suites", [&] {
         context->run(ep);
 
-        expect().nothing();
+        expect(context->isFailed()).toBeFalse();
     });
 
-    it("should do nothing when it has empty NORMAL suites", [&] {
-        context->addSuite(__FILE__, __LINE__, NORMAL, "one", [] {});
-        context->addSuite(__FILE__, __LINE__, NORMAL, "two", [] {});
+    it("should run lambdas twice in NORMAL suites", [&] {
+        int one = 0;
+        int two = 0;
+
+        context->addSuite(__FILE__, __LINE__, NORMAL, "one", [&] {
+            ++one;
+        });
+        context->addSuite(__FILE__, __LINE__, NORMAL, "two", [&] {
+            ++two;
+        });
         context->run(ep);
 
-        expect().nothing();
+        expect(context->isFailed()).toBeFalse();
+        expect(one == 2).toBeTrue();
+        expect(two == 2).toBeTrue();
     });
 
-    it("should run NORMAL tests when defined in NORMAL suites", [&] {
-        int testCount = 0;
+    it("should run lambdas once in NORMAL tests in NORMAL suites", [&] {
+        int oneOfOne = 0;
+        int twoOfOne = 0;
+        int oneOfTwo = 0;
 
         context->addSuite(__FILE__, __LINE__, NORMAL, "one", [&] {
             context->addTest(__FILE__, __LINE__, NORMAL, "one of one", [&] {
-                ++testCount;
-
-                context->doExpect(__FILE__, __LINE__).nothing();
+                ++oneOfOne;
             });
             context->addTest(__FILE__, __LINE__, NORMAL, "two of one", [&] {
-                ++testCount;
-
-                context->doExpect(__FILE__, __LINE__).nothing();
+                ++twoOfOne;
             });
         });
         context->addSuite(__FILE__, __LINE__, NORMAL, "two", [&] {
-            context->addTest(__FILE__, __LINE__, NORMAL, "one of one", [&] {
-                ++testCount;
-
-                context->doExpect(__FILE__, __LINE__).nothing();
+            context->addTest(__FILE__, __LINE__, NORMAL, "one of two", [&] {
+                ++oneOfTwo;
             });
         });
         context->run(ep);
 
-        expect(testCount == 3).toBeTrue();
+        expect(context->isFailed()).toBeFalse();
+        expect(oneOfOne == 1).toBeTrue();
+        expect(twoOfOne == 1).toBeTrue();
+        expect(oneOfTwo == 1).toBeTrue();
+    });
+
+    it("should run NORMAL tests in nested NORMAL suites", [&] {
+        int nested = 0;
+
+        context->addSuite(__FILE__, __LINE__, NORMAL, "grandfather", [&] {
+            context->addSuite(__FILE__, __LINE__, NORMAL, "father", [&] {
+                context->addSuite(__FILE__, __LINE__, NORMAL, "son", [&] {
+                    context->addTest(__FILE__, __LINE__, NORMAL, "nested", [&] {
+                        ++nested;
+                    });
+                });
+            });
+        });
+        context->run(ep);
+
+        expect(context->isFailed()).toBeFalse();
+        expect(nested == 1).toBeTrue();
     });
 });
